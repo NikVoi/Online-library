397d8fd61574d37278c7ba4b4c63405128141950
Nik_Voi <tivkaipivka@gmail.com>
2023-06-15 10:40:24 +0300

    fix: name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './deploying-vite-project-exemple/',
+  base: './Task_2/',
   plugins: [react()],
 })
